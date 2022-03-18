d573e4b316e1dca00357b075d9f4be068c294bcd
Pierre Deveix <deveixpierre@gmail.com>
2022-03-18 10:21:48 +0100

    fetching url

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,17 @@
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
 console.log(id);
-console.log('toto');
+
+
+function fetchProduct(id) {
+  fetch("http://localhost:4000/api/products/" + id)
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+    } else if (response.id === undefined) {
+      throw new Error("This product id does not exist");
+    } else {
+      throw new Error("Network response failed");
+    }
+  })
+}
